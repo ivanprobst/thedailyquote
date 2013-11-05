82684d686239f9f8e3da992559d17ded0c0d0a7e
Ivan Probst <ivan.probst@gmail.com>
2013-11-05 15:41:37 +0100

    restructuring a bit, though not yet sure of efficacy

---
+++ assets/scripts/social.js
@@ -0,0 +1,65 @@
+module.exports = {
+	updateAll : function(post){
+	
+	},
+	
+	// Posting stuff to twitter
+	updateTwitterStatus : function(post){
+		var Twit = require('twit')
+
+		var T = new Twit({
+			consumer_key:         ''
+		  , consumer_secret:      ''
+		  , access_token:         ''
+		  , access_token_secret:  ''
+		});
+		
+		// Send the request
+		T.post('statuses/update', { status: post }, function(err, reply) {
+			if(err) console.log("error: "+err);
+			else console.log("reply: "+reply);
+		});
+	},
+
+	// Posting stuff on facebook
+	updateFacebookPage : function(post){
+		var request = require('request');
+
+		var url = 'https://graph.facebook.com/1410710079162036/feed';
+		var params = {
+			access_token: '',
+			message: post
+		};
+
+		// Send the request
+		request.post({url: url, qs: params}, function(err, resp, body) {
+			  // Handle any errors that occur
+			  if (err) return console.error("Error occured: ", err);
+			  body = JSON.parse(body);
+			  if (body.error) return console.error("Error returned from facebook: ", body.error);
+
+			  // Generate output
+			  var output = '<p>Message has been posted to your feed. Here is the id generated:</p>';
+			  output += '<pre>' + JSON.stringify(body, null, '\t') + '</pre>';
+			  console.log(output);
+		});
+	},
+	
+	// Init rss feed
+	initRSS : function(){
+		var nb = 1; // replace by real content, title, author
+		var date = new Date(); // replace by quote published time
+		var RSS = require('rss');
+		var feed = new RSS({"title":"The Quote Tribune","description":"Your daily inspirational fix","feed_url":"http://thequotetribune.com/rss.xml","site_url":"http://thequotetribune.com"});
+		feed.item({"title":"Post nb "+nb,"description":"awesome content nb "+nb,"url":"http://thequotetribune.com?id="+nb,"guid":"id"+nb,"date":date.toDateString()+", "+date.getHours()+":"+date.getMinutes(),"categories":["cat1"],"author":"Marcus Aurelius"})
+		console.log('...rss feed set up');
+		return feed.xml();
+	},
+	
+	// Posting stuff on rss
+	updateRSS : function(){
+		rssXML = feed.item({"title":"Post nb "+nb,"description":"awesome content nb "+nb,"url":"thequotetribune.com?id="+nb,"guid":"id"+nb,"date":date.toDateString()+", "+date.getHours()+":"+date.getMinutes(),"categories":["cat1"],"author":"Marcus Aurelius"}).xml();
+		nb++;
+	}
+};
+
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-/*
-var quote =		{"id":"1234","text":"Begin - to begin is half the work, let half still remain; again begin this, and thou wilt have finished.","quotesomeUrl":"http://quoteso.me/q/9323","authorID":"marcus_aurelius"}
-var author =	{"name":"Marcus Aurelius","wikipediaRef":"Marcus_Aurelius","quotesomeUrl":"https://www.quotesome.com/authors/marcus-aurelius/quotes","photoPath":"https://s3-eu-west-1.amazonaws.com/thequotetribune/photos/marcus_aurelius.jpg","photoWidth":3256, "photoHeight":1600, "positionLeft":2, "positionTop":5, "directionSlide":"left", "blockWidth":35, "blockFontSize":48, "blockFontColor":"fff", "barsColor":"fff"};
-
-var quote =		{"id":"2345","text":"Great minds discuss ideas. Average minds discuss events. Small minds discuss people.","quotesomeUrl":"http://quoteso.me/q/224850","authorID":"eleanor_roosevelt"}
-var author =	{"name":"Eleanor Roosevelt","wikipediaRef":"Eleanor_Roosevelt","quotesomeUrl":"https://www.quotesome.com/authors/eleanor-roosevelt/quotes","photoPath":"https://s3-eu-west-1.amazonaws.com/thequotetribune/photos/eleanor_roosevelt.jpg","photoWidth":2665, "photoHeight":1203, "positionRight":3, "positionBottom":5, "directionSlide":"left", "blockWidth":35, "blockFontSize":48, "blockFontColor":"000", "blockBackgroundColor":"fff", "barsColor":"fff"};
-*/
-
-var CONST = require('./assets/scripts/CONST.js');
+// external modules
 var http = require('http');
 var fs = require('fs');
 var mongo = require('mongodb').MongoClient;
 var geoip = require('geoip-lite');
 
+// internal modules
+var social = require('./assets/scripts/social.js');
+var CONST = require('./assets/scripts/CONST.js');
+
+// varz
 var quote = {};
 var author = {};
-var extensionMapping = {
-						".png":"image/png",".jpg":"image/jpg",".gif":"image/gif",".ico":"image/x-icon",
-						".js":"text/javascript",".css":"text/css",
-						".html":"text/html"
-					};
+var rssXML = '';
+
+// Launch init
+rssXML = social.initRSS();
+console.log('...server running at http://127.0.0.1:8124/');
+//trig social update timer
 
 http.createServer(function (request, response) {
 	console.log('# client asked for: '+request.url);
@@ -37,12 +35,10 @@ http.createServer(function (request, response) {
 		return;
 	}
 	
-	// if asked other, set header...
-	var requestExtension = extensionMapping[request.url.match(/\.[0-9a-z]+$/)];
+	// if asked other, set header and stream the file
+	var requestExtension = CONST.extension_map[request.url.match(/\.[0-9a-z]+$/)];
 	if(!requestExtension) requestExtension = 'text/plain';
 	response.writeHead(200, {'Content-Type': requestExtension});
-	
-	// ...and stream the file
 	console.log('... serving: '+request.url);
 	var file = fs.createReadStream(('.'+request.url));
 	file.pipe(response);
@@ -53,7 +49,6 @@ http.createServer(function (request, response) {
 		response.end();
 	});
 }).listen(8124);
-console.log('Server running at http://127.0.0.1:8124/');
 
 // initialize the home
 function initHome(request, response){
@@ -109,7 +104,7 @@ function initHome(request, response){
 function buildHome(response){
 	// and build that page
 	var htmlPage = '';
-	var file = fs.createReadStream('index.html');
+	var file = fs.createReadStream('assets/templates/index.html');
 	file.on('data', function(data){htmlPage = htmlPage + data;});
 	file.on('error', function(err){console.error("no index file found...");});
 	file.on('end', function(err){
@@ -160,57 +155,3 @@ function buildHome(response){
 	});
 }
 
-
-// Posting stuff to twitter
-function updateTwitterStatus(post){
-	var Twit = require('twit')
-
-	var T = new Twit({
-		consumer_key:         ''
-	  , consumer_secret:      ''
-	  , access_token:         ''
-	  , access_token_secret:  ''
-	});
-	
-	// Send the request
-	T.post('statuses/update', { status: post }, function(err, reply) {
-		if(err) console.log("error: "+err);
-		else console.log("reply: "+reply);
-	});
-}
-
-// Posting stuff on facebook
-function updateFacebookPage(post){
-	var request = require('request');
-
-	var url = 'https://graph.facebook.com/1410710079162036/feed';
-	var params = {
-		access_token: '',
-		message: post
-	};
-
-	// Send the request
-	request.post({url: url, qs: params}, function(err, resp, body) {
-		  // Handle any errors that occur
-		  if (err) return console.error("Error occured: ", err);
-		  body = JSON.parse(body);
-		  if (body.error) return console.error("Error returned from facebook: ", body.error);
-
-		  // Generate output
-		  var output = '<p>Message has been posted to your feed. Here is the id generated:</p>';
-		  output += '<pre>' + JSON.stringify(body, null, '\t') + '</pre>';
-		  console.log(output);
-	});
-}
-
-// init rss feed
-var nb = 1; // replace by real content, title, author
-var date = new Date(); // replace by quote published time
-var RSS = require('rss');
-var feed = new RSS({"title":"The Quote Tribune","description":"Your daily inspirational fix","feed_url":"http://thequotetribune.com/rss.xml","site_url":"http://thequotetribune.com"});
-var rssXML = feed.item({"title":"Post nb "+nb,"description":"awesome content nb "+nb,"url":"http://thequotetribune.com?id="+nb,"guid":"id"+nb,"date":date.toDateString()+", "+date.getHours()+":"+date.getMinutes(),"categories":["cat1"],"author":"Marcus Aurelius"}).xml();
-// Posting stuff on rss
-function updateRSS(){
-	rssXML = feed.item({"title":"Post nb "+nb,"description":"awesome content nb "+nb,"url":"thequotetribune.com?id="+nb,"guid":"id"+nb,"date":date.toDateString()+", "+date.getHours()+":"+date.getMinutes(),"categories":["cat1"],"author":"Marcus Aurelius"}).xml();
-	nb++;
-}
