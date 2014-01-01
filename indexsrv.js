23fe0dd721301c877dd055712bba583467a8262a
Ivan Probst <ivan.probst@gmail.com>
2014-01-01 18:39:01 +0100

    server-side mobile detection test

--- indexsrv.js
+++ indexsrv.js
@@ -29,6 +29,25 @@ console.log('# running server on http://127.0.0.1:8124/');
 http.createServer(function (request, response) {
 	console.log('# index srv asked for: '+request.url);
 
+	var ua = request.headers['user-agent'];
+    var $ = {};
+	if (/mobile/i.test(ua))
+	    $.Mobile = true;
+	if (/like Mac OS X/.test(ua)) {
+	    $.iOS = /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/.exec(ua)[2].replace(/_/g, '.');
+	    $.iPhone = /iPhone/.test(ua);
+	    $.iPad = /iPad/.test(ua);
+	}
+	if (/Android/.test(ua))
+	    $.Android = /Android ([0-9\.]+)[\);]/.exec(ua)[1];
+	if (/webOS\//.test(ua))
+	    $.webOS = /webOS\/([0-9\.]+)[\);]/.exec(ua)[1];
+	if (/(Intel|PPC) Mac OS X/.test(ua))
+	    $.Mac = /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/.exec(ua)[2].replace(/_/g, '.') || true;
+	if (/Windows NT/.test(ua))
+	    $.Windows = /Windows NT ([0-9\._]+)[\);]/.exec(ua)[1];
+	console.log('test:'+$.Mobile+', and: '+$.Mac);
+
 	// if home page asked, serve home page...
 	if(request.url == '/'){
 		console.log('...processing index page request');
